22d879436e54697672315ec60879e82b0c444d7d
Vinicius Bortoletto <vinicius@pop-os.localdomain>
2022-06-21 16:16:58 -0300

    exercicio 3

---
+++ m1-fundamentos/b4-introdução-a-javascript-e-lógica-da-programação/d1-primeiros-passos/exercicio-3/script.js
@@ -0,0 +1,9 @@
+const score = 59;
+
+if (score >= 80) {
+  console.log("Parabéns, você foi aprovada(o)");
+} else if (score < 80 && score >= 60) {
+  console.log("Você está na nossa lista de espera.");
+} else {
+  console.log("Você foi reprovado!");
+}
