e5fa62620074a1513fb84dfaa022299d41720709
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-06-28 18:43:49 -0300

    exercicios 3, requisito 3

--- m1-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-dom-e-seletores/exercicio-3/script.js
+++ m1-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-dom-e-seletores/exercicio-3/script.js
@@ -5,7 +5,6 @@
     - document.getElementsByTagName()
    
     
-   Crie uma função que mude a cor do quadrado vermelho para branco.
    Crie uma função que corrija o texto da tag <h1>.
    Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
    Crie uma função que exiba o conteúdo de todas as tags <p> no console.
@@ -20,3 +19,6 @@ for (p of pElmts) {
 
 const mainContent = document.getElementsByClassName("main-content");
 mainContent[0].style.backgroundColor = "rgb(76, 164, 109)";
+
+const centerContent = document.getElementsByClassName("center-content");
+centerContent[0].style.backgroundColor = "white";
