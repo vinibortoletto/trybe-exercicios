d664fe75073f264d6a001acb54558b710eb63486
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-06-28 18:44:57 -0300

    exercicios 3, requisito 4

--- m1-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-dom-e-seletores/exercicio-3/script.js
+++ m1-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-dom-e-seletores/exercicio-3/script.js
@@ -5,7 +5,6 @@
     - document.getElementsByTagName()
    
     
-   Crie uma função que corrija o texto da tag <h1>.
    Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
    Crie uma função que exiba o conteúdo de todas as tags <p> no console.
 */
@@ -22,3 +21,6 @@ mainContent[0].style.backgroundColor = "rgb(76, 164, 109)";
 
 const centerContent = document.getElementsByClassName("center-content");
 centerContent[0].style.backgroundColor = "white";
+
+const title = document.getElementsByClassName("title");
+title[0].innerText = "Exercício 5.1 - JavaScript";
