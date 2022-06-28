fc76166e60b2dae986750ad88a0eb7ae1942b046
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-06-28 18:46:22 -0300

    exercicios 3, requisito 5

--- m1-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-dom-e-seletores/exercicio-3/script.js
+++ m1-fundamentos/b5-javascript-dom-eventos-e-web-storage/d1-dom-e-seletores/exercicio-3/script.js
@@ -5,7 +5,6 @@
     - document.getElementsByTagName()
    
     
-   Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
    Crie uma função que exiba o conteúdo de todas as tags <p> no console.
 */
 
@@ -14,6 +13,9 @@ for (p of pElmts) {
   if (p.innerText.includes("-")) {
     p.innerText = "Me vejo como um excelente desenvolvedor.";
   }
+
+  p.style.textTransform = "uppercase";
+  p.style.color = "black";
 }
 
 const mainContent = document.getElementsByClassName("main-content");
