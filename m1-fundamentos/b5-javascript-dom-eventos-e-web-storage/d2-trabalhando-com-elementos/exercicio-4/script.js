6c87dba32d95d53a651f8db409fda60fc78f1401
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-06-30 13:23:14 -0300

    exercicios 4, requisito 4

--- m1-fundamentos/b5-javascript-dom-eventos-e-web-storage/d2-trabalhando-com-elementos/exercicio-4/script.js
+++ m1-fundamentos/b5-javascript-dom-eventos-e-web-storage/d2-trabalhando-com-elementos/exercicio-4/script.js
@@ -1,16 +1,20 @@
-const body = document.body;
+const bodyElement = document.body;
 
 // Requisito 1
-const title = document.createElement("h1");
-title.innerText = "Exercício 5.2 - JavaScript DOM";
-body.appendChild(title);
+const titleElement = document.createElement("h1");
+titleElement.innerText = "Exercício 5.2 - JavaScript DOM";
+bodyElement.appendChild(titleElement);
 
 // Requisito 2
 const mainElement = document.createElement("main");
 mainElement.classList = "main-content";
-body.appendChild(mainElement);
+bodyElement.appendChild(mainElement);
 
 // Requisito 3
-const sectionCenter = document.createElement("section");
-sectionCenter.classList = "center-content";
-mainElement.appendChild(sectionCenter);
+const sectionCenterElement = document.createElement("section");
+sectionCenterElement.classList = "center-content";
+mainElement.appendChild(sectionCenterElement);
+
+// Requisito 4
+const pElement = document.createElement("p");
+sectionCenterElement.appendChild(pElement);
