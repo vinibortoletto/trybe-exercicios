3402298f7411334d87de5f1638930771c5eb6081
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-06-30 16:06:53 -0300

    exercicio 1, requisito 3

--- m1-fundamentos/b5-javascript-dom-eventos-e-web-storage/d3-eventos/exercicio-1/main.js
+++ m1-fundamentos/b5-javascript-dom-eventos-e-web-storage/d3-eventos/exercicio-1/main.js
@@ -35,6 +35,12 @@ input.addEventListener("keypress", (e) => updateBoxText(e));
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+function redirectToPortfolio() {
+  window.open("https://vinibortoletto.netlify.app/");
+}
+
+myWebpage.style.cursor = "pointer";
+myWebpage.addEventListener("dblclick", redirectToPortfolio);
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
