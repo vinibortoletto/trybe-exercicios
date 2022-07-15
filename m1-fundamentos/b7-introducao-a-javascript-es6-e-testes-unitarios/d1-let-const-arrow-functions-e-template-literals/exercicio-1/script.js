1e55976c2b53c889ea14f78bff6afc944979e17a
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-07-15 15:53:16 -0300

    exercicio 1

--- m1-fundamentos/b7-introducao-a-javascript-es6-e-testes-unitarios/d1-let-const-arrow-functions-e-template-literals/exercicio-1/script.js
+++ m1-fundamentos/b7-introducao-a-javascript-es6-e-testes-unitarios/d1-let-const-arrow-functions-e-template-literals/exercicio-1/script.js
@@ -17,15 +17,8 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 const sortArray = () => {
   const sortedArray = oddsAndEvens.sort((a,b) => {return a - b});
-  let stringOfNumbers = ''
-
-  for(let i = 0; i < sortedArray.length; i += 1) {
-    i + 1 === sortedArray.length 
-    ? stringOfNumbers += `${sortedArray[i]}`
-    : stringOfNumbers += `${sortedArray[i]}, `
-  }
-
-  console.log(`Os números ${stringOfNumbers} se encontram ordenados de forma crescente!`);
+  console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente!`);
 }
 
 sortArray()
+
