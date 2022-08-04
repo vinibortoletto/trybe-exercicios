edb67f21a213bda4db61f026f1b242e840da324c
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-08-04 20:21:13 -0300

    exercicios 5

---
+++ m2-front-end/b9-javascript-e-testes-assincronos/d1-javascript-assincrono-e-callbacks/exercicio-5/script.js
@@ -0,0 +1,29 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9) / 5 + 32;
+
+const temperatureInFahrenheit = (temperature) => {
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+};
+
+const greet = (temperature) => {
+  console.log(
+    `Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`
+  );
+};
+
+const sendMarsTemperature = (callback) => {
+  setTimeout(() => {
+    const temperature = getMarsTemperature();
+    console.log(`Mars temperature is: ${temperature} degree Celsius`);
+    callback(temperature);
+  }, messageDelay());
+};
+
+sendMarsTemperature(temperatureInFahrenheit);
+sendMarsTemperature(greet);
