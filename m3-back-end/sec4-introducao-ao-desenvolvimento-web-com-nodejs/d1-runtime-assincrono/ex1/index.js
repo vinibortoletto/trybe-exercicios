22f268a5623a7c03d89f4ece9f4cdefc315bb532
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-12-06 20:01:22 -0300

    ex 5

--- m3-back-end/sec4-introducao-ao-desenvolvimento-web-com-nodejs/d1-runtime-assincrono/ex1/index.js
+++ m3-back-end/sec4-introducao-ao-desenvolvimento-web-com-nodejs/d1-runtime-assincrono/ex1/index.js
@@ -1,13 +1,19 @@
 const { calculateBMI } = require('./bmi');
 const { questionInt, questionFloat } = require('readline-sync');
 
-const weightInKilos = questionFloat('What is your weight in kilos?');
-const heightInCentimeters = questionInt('What is your height in centimeters?');
-
 const main = () => {
-  80;
+  const weightInKilos = questionFloat('Qual seu peso em kilos?');
+  const heightInCentimeters = questionInt('Qual sua altura em centímetros?');
+
   const bmi = calculateBMI(weightInKilos, heightInCentimeters);
   console.log(`BMI: ${bmi.toFixed(2)}`);
+
+  if (bmi < 18.5) return console.log('Abaixo do peso (magreza)');
+  if (bmi >= 18.5 && bmi <= 24.9) return console.log('Peso normal');
+  if (bmi >= 25 && bmi <= 29.9) return console.log('Acima do peso (sobrepeso)');
+  if (bmi >= 30 && bmi <= 34.9) return console.log('Obesidade grau I');
+  if (bmi >= 35 && bmi <= 39.9) return console.log('Obesidade grau II');
+  if (bmi >= 40) return console.log('Obesidade grau III e IV');
 };
 
 main();
