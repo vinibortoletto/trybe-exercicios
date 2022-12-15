154a36cc9f307d440b83dceb9d709ccd2fafc191
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-12-15 13:57:59 -0300

    feat: validate createdAt

--- m3-back-end/sec4-introducao-ao-desenvolvimento-web-com-nodejs/d4-express-e-middlewares/ecoturism-app/src/middleware/validateDescription.js
+++ m3-back-end/sec4-introducao-ao-desenvolvimento-web-com-nodejs/d4-express-e-middlewares/ecoturism-app/src/middleware/validateDescription.js
@@ -1,11 +1,18 @@
 module.exports = (req, res, next) => {
   const { description } = req.body;
+
   const errorMessage = {
     message: 'O campo description é obrigatório',
   };
 
   if (!description) {
     return res.status(400).json(errorMessage);
+  }
+
+  if (!description.createdAt) {
+    return res.status(400).json({
+      message: `O campo createdAt é obrigatório`,
+    });
   }
 
   return next();
