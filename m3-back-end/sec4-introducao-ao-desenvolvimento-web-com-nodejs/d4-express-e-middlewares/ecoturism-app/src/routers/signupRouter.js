9ee3de1e51acfae353b4276fc3274375f278d538
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-12-15 16:20:53 -0300

    feat: insert new user

--- m3-back-end/sec4-introducao-ao-desenvolvimento-web-com-nodejs/d4-express-e-middlewares/ecoturism-app/src/routers/signupRouter.js
+++ m3-back-end/sec4-introducao-ao-desenvolvimento-web-com-nodejs/d4-express-e-middlewares/ecoturism-app/src/routers/signupRouter.js
@@ -1,13 +1,17 @@
 const { Router } = require('express');
+const generateToken = require('../utils/generateToken');
 const router = Router();
 
 router.post('/', async function (req, res) {
   const { email, password, firstName, phone } = req.body;
   const errorMessage = { message: 'Campos ausentes!' };
+  const randomToken = generateToken();
 
   if (!email || !password || !firstName || !phone) {
     return res.status(401).json(errorMessage);
   }
+
+  return res.status(200).json({ token: randomToken });
 });
 
 module.exports = router;
