719dbc64c58507b5f1ad6283f8923ef52909600f
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-12-15 01:06:02 -0300

    feat: post request

--- m3-back-end/sec4-introducao-ao-desenvolvimento-web-com-nodejs/d4-express-e-middlewares/ecoturism-app/src/app.js
+++ m3-back-end/sec4-introducao-ao-desenvolvimento-web-com-nodejs/d4-express-e-middlewares/ecoturism-app/src/app.js
@@ -4,4 +4,9 @@ require('express-async-errors');
 const app = express();
 app.use(express.json());
 
+app.post('/activities', async function (req, res) {
+  const successMessage = { message: 'Atividade cadastrada com sucesso!' };
+  return res.status(201).json(successMessage);
+});
+
 module.exports = { app };
