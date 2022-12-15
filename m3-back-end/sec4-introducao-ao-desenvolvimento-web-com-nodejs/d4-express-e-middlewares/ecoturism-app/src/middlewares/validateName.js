fe14e0985793d4484d8f4544b064985c7117a34b
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-12-15 01:42:59 -0300

    feat: validate if name length is less than 4

--- m3-back-end/sec4-introducao-ao-desenvolvimento-web-com-nodejs/d4-express-e-middlewares/ecoturism-app/src/middlewares/validateName.js
+++ m3-back-end/sec4-introducao-ao-desenvolvimento-web-com-nodejs/d4-express-e-middlewares/ecoturism-app/src/middlewares/validateName.js
@@ -5,5 +5,11 @@ module.exports = (req, res, next) => {
     return res.status(400).json({ message: 'O campo name é obrigatório' });
   }
 
+  if (name.length < 4) {
+    return res
+      .status(400)
+      .json({ message: 'O campo name deve ter pelo menos 4 caracteres' });
+  }
+
   return next();
 };
