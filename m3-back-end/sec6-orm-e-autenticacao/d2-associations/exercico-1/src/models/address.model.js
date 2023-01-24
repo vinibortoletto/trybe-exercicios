d83a4c75fbd8c0786f73f301472a367394e870d6
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-01-24 16:57:09 -0300

    exercicio do course

--- m3-back-end/sec6-orm-e-autenticacao/d2-associations/exercico-1/src/models/address.model.js
+++ m3-back-end/sec6-orm-e-autenticacao/d2-associations/exercico-1/src/models/address.model.js
@@ -2,7 +2,11 @@ module.exports = (sequelize, DataTypes) => {
   const Address = sequelize.define(
     'Address',
     {
-      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
       city: DataTypes.STRING,
       street: DataTypes.STRING,
       number: DataTypes.INTEGER,
